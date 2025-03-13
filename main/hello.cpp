ea873bd419478d34b724bd68fc7fb9143071c85e
Keerthan Shenoy <shenoyk2003@gmail.com>
2025-03-13 16:01:09 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int main(void) {
   cout << "PES1UG22CS281\n";
-  cout << "Keerthan Shenoy\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
